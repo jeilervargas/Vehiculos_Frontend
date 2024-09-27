f6b21ac7a2a2b7d2ba1cdac0f9d0187e51ac4f87
Jeiler Vargas <jeiler.a.vargas@gmail.com>
2024-09-27 18:34:02 -0500

    Commit 4: Se creo el paquete dto con las clases ConsultaRequestDTO y ConsultaResponseDTO. Además, se creo el paquete config con la clase RestTemplateConfig

---
+++ src/main/java/pe/edu/cibertec/vehiculos_frontend/dto/ConsultaRequestDTO.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.vehiculos_frontend.dto;
+
+public record ConsultaRequestDTO(String matricula) {
+}
---
+++ src/main/java/pe/edu/cibertec/vehiculos_frontend/dto/ConsultaResponseDTO.java
@@ -0,0 +1,5 @@
+package pe.edu.cibertec.vehiculos_frontend.dto;
+
+public record ConsultaResponseDTO(String codigo, String mensaje,  String marca, String modelo
+        ,String nroAsientos, String precio, String color) {
+}
