7ddf67ee2082ff9a3b8d8ba92a99ebb22ebf2ff1
Jeiler Vargas <jeiler.a.vargas@gmail.com>
2024-09-26 21:05:26 -0500

    Primer commit: creación del proyecto vehiculos-frontend

---
+++ src/main/java/pe/edu/cibertec/vehiculos_frontend/VehiculosFrontendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.vehiculos_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiculosFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiculosFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/vehiculos_frontend/VehiculosFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.vehiculos_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiculosFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
