0fcc00636f543b0dbbbf67ebff9947b83e19d166
Jeiler Vargas <jeiler.a.vargas@gmail.com>
2024-09-27 17:27:13 -0500

    Commit 3: Se añadieron las hojas de estilo inicio.css y principal.css. Se le dio forma al inicio.html con bootstrap. Se corrigio un importación erronea en ConsultaController

--- src/main/java/pe/edu/cibertec/vehiculos_frontend/controller/ConsultaController.java
+++ src/main/java/pe/edu/cibertec/vehiculos_frontend/controller/ConsultaController.java
@@ -1,7 +1,7 @@
 package pe.edu.cibertec.vehiculos_frontend.controller;
 
-import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
