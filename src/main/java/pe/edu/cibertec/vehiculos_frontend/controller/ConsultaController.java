18aef18e00ba2d33b94d0baa9928680d7e8ed776
Jeiler Vargas <jeiler.a.vargas@gmail.com>
2024-09-27 23:54:11 -0500

    Commit final: fin del proyecto

--- src/main/java/pe/edu/cibertec/vehiculos_frontend/controller/ConsultaController.java
+++ src/main/java/pe/edu/cibertec/vehiculos_frontend/controller/ConsultaController.java
@@ -1,16 +1,21 @@
 package pe.edu.cibertec.vehiculos_frontend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 import pe.edu.cibertec.vehiculos_frontend.viewmodel.ConsultaModel;
 
 @Controller
 @RequestMapping("/consulta")
 public class ConsultaController {
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @GetMapping("/inicio")
     public String inicio(Model model) {
@@ -24,7 +29,7 @@ public class ConsultaController {
     public String consultar(@RequestParam("matricula") String matricula, Model model) {
 
         // condiciones de validación de campo matricula
-        if (matricula == null || matricula.trim().length() == 0 || matricula.trim().length() > 6) {
+        if (matricula == null || matricula.trim().length() == 0 || matricula.trim().length() > 7) {
             ConsultaModel consultaModel = new ConsultaModel("01", "Error: Ingrese una matricula válida.",
                     "", "", "", "", "");
             model.addAttribute("consultaModel", consultaModel);
